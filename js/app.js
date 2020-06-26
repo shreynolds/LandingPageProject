be3616b8155eb3b54eb381e96137ab002f91ffad
Sophie Reynolds <shreynolds101@gmail.com>
2020-06-26 12:37:27 -0700

    Fixed formatting issues

--- js/app.js
+++ js/app.js
@@ -27,43 +27,10 @@ let toTop = document.getElementsByClassName('to_top');
  * 
 */
 
-
-
 /**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-
-for(let x = 0; x < sections.length; x++){
-    listItem = document.createElement('li');
-    let sectionName = sections[x].dataset.nav;
-    listItem.innerText = sectionName;
-    listItem.className = 'menu__link';
-    listItem.id = 'nav_' + sections[x].id;
-    console.log(listItem);
-    listItem.addEventListener('click', function(){ 
-        sections[x].scrollIntoView({behavior:'smooth'});
-    })
-    navMenu.appendChild(listItem);
-}
-
-document.addEventListener('scroll', function(el){
-    for (section of sections){
-        let height = window.innerHeight;
-        let top= section.getBoundingClientRect().top;
-        let bottom = section.getBoundingClientRect().bottom;
-        let avg = (top + bottom) / 2;
-        if(avg < height && top>0){
-            setActiveClass(section.id);
-        }
-    }
-})
-
+ * Sets the class with the passed ID as active, and sets the others as inactive
+ * @param {string} id The ID of the class that should be the active class
+ */
 function setActiveClass(id){
     for (section of sections){
         if (section.id == id){
@@ -76,26 +43,57 @@ function setActiveClass(id){
 }
 
 
-for (button of toTop){
-    button.addEventListener('click', function(){
-        window.scrollTo({top: 0 , behavior: 'smooth'});
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+/**
+ * From the sections, builds the navigation bar and adds scrolling event listeners
+ */
+function buildNav(){
+    for(let x = 0; x < sections.length; x++){
+        listItem = document.createElement('li');
+        let sectionName = sections[x].dataset.nav;
+        listItem.innerText = sectionName;
+        listItem.className = 'menu__link';
+        listItem.id = 'nav_' + sections[x].id;
+        console.log(listItem);
+        listItem.addEventListener('click', function(){ 
+            sections[x].scrollIntoView({behavior:'smooth'});
+        })
+        navMenu.appendChild(listItem);
+    }
+}
+
+/**
+ * On scroll, checks for the section that is within the pasge and sets it as the active class
+ */
+function scrollListener(){
+    document.addEventListener('scroll', function(el){
+        for (section of sections){
+            let height = window.innerHeight;
+            let top = section.getBoundingClientRect().top;
+            let bottom = section.getBoundingClientRect().bottom;
+            let avg = (top + bottom) / 2;
+            if(avg < height && top>0){
+                setActiveClass(section.id);
+            }
+        }
     })
 }
 
-navMenu.addEventListener('click', function(el){
-    idClicked = el.target.id;
-    sectionClicked = document.getElementById(idClicked);
-    console.log(el.target.id);
-    console.log(sectionClicked);
-})
-
-
-// Add class 'active' to section when near top of viewport
-
-
-
-// Scroll to anchor ID using scrollTO event
-
+/**
+ * Adds an event listener to scroll to the top for the buttons
+ */
+function buttonListener(){
+    for (button of toTop){
+        button.addEventListener('click', function(){
+            window.scrollTo({top: 0 , behavior: 'smooth'});
+        })
+    }
+}
 
 /**
  * End Main Functions
@@ -103,8 +101,7 @@ navMenu.addEventListener('click', function(el){
  * 
 */
 
-// Build menu 
+buildNav();
+scrollListener();
+buttonListener();
 
-// Scroll to section on link click
-
-// Set sections as active
